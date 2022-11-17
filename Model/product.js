8d18aab3dafa1d6ef43b9308cb051cbae06df8f7
Shamnad KK <shamnadchemmu702@gmail.com>
2022-11-17 13:51:55 +0530

    product model changed

--- Model/product.js
+++ Model/product.js
@@ -1,61 +1,72 @@
 const { default: mongoose } = require('mongoose')
-const {AutoIncrement} = require('../config/connection')
+const { AutoIncrement } = require('../config/connection')
 
 const schema = require('mongoose').Schema
-const {ObjectId} = mongoose.Schema.Types
+const { ObjectId } = mongoose.Schema.Types
 
 
 const productModel = new schema({
 
-    name : {
-        type : String,
-        required : true
+    name: {
+        type: String,
+        required: true
     },
     category: {
-        type: ObjectId,
-        required: true,
-        ref : 'category',
-        subCategory : {
-                type : ObjectId,
-                ref : "subCategory"
+        id: {
+            type: ObjectId,
+            required: true,
+            ref: 'category',
+            subCategory: {
+                type: ObjectId,
+                ref: "subCategory"
             }
+        }
     },
-    price : {
-        type : Number,
-        required : true
+    price: {
+        type: Number,
+        required: true
     },
-    size : {
-        type : Array,
-        required : true
+    size: {
+        type: Array,
+        required: true
     },
-    colors : {
-        type : Array,
-        default : []
-    },
-    images : {
-        type:Array,
-        default : []
-    },
-    inventory : {
-        type : Number,
-        required : true
-    },
-    description : {
-        type : String,
-        required : true
-    },
-    ratings : [
+    colors: [
         {
-            user : {
-                type : ObjectId,
-                ref : "users"
+            color: {
+                type: String,
+                required: true
             },
-            rating : {
-                type : Number
+            images: [
+                {
+                type: String,
+                required: true
+            }
+        ]
+    }
+    ],
+    inventory: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    offer:{
+         type : Number
+    },
+    ratings: [
+        {
+            user: {
+                type: ObjectId,
+                ref: "users"
+            },
+            rating: {
+                type: Number
             }
         }
     ]
-    },{timestamps:true})
+}, { timestamps: true })
 
 // categoryModal.plugin(AutoIncrement,{inc_field : "id"});
 
