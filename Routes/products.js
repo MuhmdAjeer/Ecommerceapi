4221b5c2b180219e7a72bc4674a4de8942a9ce3a
Shamnad KK <shamnadchemmu702@gmail.com>
2022-11-24 17:36:21 +0530

    fixed getoneproduct issue

--- Routes/products.js
+++ Routes/products.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { addProduct, getAllProducts , getProduct, addTocart,getCart} = require('../controller/product.controller');
+const { verify } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.route('/')
